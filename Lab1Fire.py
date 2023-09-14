6756f09ace532e4f49010d7c7f0bea8f9bcb5dee
Derek Scholten <dscholte@umich.edu>
2023-09-14 11:21:46 -0400

    Update Lab1Fire.py

--- Lab1Fire.py
+++ Lab1Fire.py
@@ -22,15 +22,15 @@ forest_cmap = ListedColormap(['tan', 'darkgreen', 'crimson'])
 
 nx, ny, numiters = 3, 3, 3 # Number of cells in X and Y direction.
 prob_spread = 1.0 # Chance to spread to adjacent cells.
-prob_bare = 0.0 # Chance of cell to start as bare patch.
+prob_bare = 0.5 # Chance of cell to start as bare patch.
 prob_start = 0.0 # Chance of cell to start on fire.
 
 
 # Create an initial grid, set all values to "2". dtype sets the value
 # type in our array to integers only.
-forest  =  np.zeros([ny,nx,numiters], dtype=int) + 2
-forest([numiters], dtype=int) ==0
+forest  =  np.zeros([numiters,ny,nx], dtype=int) + 2
 
+ 
 # Loop in the "x" direction:
 for i in range(nx):
     # Loop in the "y" direction:
@@ -42,23 +42,38 @@ for i in range(nx):
 # Set the center cell to "burning":
 forest [1,1] = 3
 
+
 #fire spread loop
 for k in range(1, numiters):
+    
+    forest[k,ny,nx] = forest[k-1,ny,nx]
     for i in range(nx):
         for j in range(ny):
-            # Roll our "dice" to see if we get a bare spot:
-            if forest(k-1,j,i)==3:
+           
+            if forest[k-1,j,i]==3:
                 
-                if forest(k-1,j,i-1)==2:
+                if forest[j,i-1]==2:
                     
                     forest[j, i-1] = 3
 
+                if forest[j-1,i]==2:
+                    
+                    forest[j-1,i] = 3
+                    
+                if forest[j,i+1]==2:
+                    
+                    forest[j, i+1] = 3
+                    
+                if forest[j+1,i]==2:
+                    
+                    forest[j+1, i] = 3
 
 
 
+'''
 fig, ax = plt.subplots(1,1)
-ax.pcolor(forest, cmap=forest_cmap, vmin=1, vmax=3)
-
+#ax.pcolor(forest, cmap=forest_cmap, vmin=1, vmax=3)
+'''
 
 
 
