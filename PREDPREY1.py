66080ad9f3c7568fb2640c9429fd6a4ca3fceb8e
Derek Scholten <dscholte@umich.edu>
2023-09-28 13:27:03 -0400

    Did some new stuff

--- PREDPREY1.py
+++ PREDPREY1.py
@@ -14,8 +14,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 plt.style.use("fivethirtyeight")
-from scipy.integrate import odeint
-from scipy.integrate import solve_ivp, ode
+
+from scipy.integrate import solve_ivp
 from dataclasses import dataclass
 
 
@@ -29,19 +29,32 @@ class modelrundata:
         if len(self.P_sol) != len(self.times):
             raise ValueError("Lists are not the same length! Doesn't make sense!")
 
-
-y0 = [10, 1]
-
 t = np.linspace(0, 50, num=1000)
 
-alpha = 1.1
-beta = 0.4
-delta = 0.1
-gamma = 0.4
+alpha = 1
+beta = 2
+delta = 1
+gamma = 3
 
 params = [alpha, beta, delta, gamma]
 
+N1initial = 0.3
+N2initial = 0.6
 
+#Competition equations
+def dNdt_comp(t, variables, alpha, beta, delta, gamma):
+    # prey population
+    x = variables[0]
+
+    # predator population
+    y = variables[1]
+    
+    dn1dt = (alpha * x * (1 - x)) - (beta * x * y)
+    dn2dt = (delta * y * (1 - y)) - (gamma * x * y)
+    
+    return (dn1dt, dn2dt)
+
+#Predator-Prey equations
 def simulation(t, variables, alpha, beta, delta, gamma):
     # prey population
     x = variables[0]
@@ -50,8 +63,8 @@ def simulation(t, variables, alpha, beta, delta, gamma):
     y = variables[1]
 
     # derivatives
-    dxdt = alpha * x - beta * x * y
-    dydt = delta * x * y - gamma * y
+    dxdt = (alpha * x) - (beta * x * y)
+    dydt = (-delta * y) + (gamma * x * y)
 
     return (dxdt, dydt)
 
@@ -68,6 +81,7 @@ def taylor_step(prey, pred, dt, a, b, c, d) -> float:
     pred_deriv = dt *  results[1]
 
     return prey + prey_deriv, pred + pred_deriv
+
 
 
 def euler_method(
@@ -93,7 +107,7 @@ def euler_method(
     solution_prey[0] = N_prey_init
     solution_pred[0] = N_pred_init
 
-    ## Run Euler Method ##
+    ## Run Euler Method with taylor_step function##
     for index, value in enumerate(times[0:-1]):
         solution_prey[index + 1], solution_pred[index + 1] = taylor_step(
             solution_prey[index], solution_pred[index], dt, a, b, c, d
@@ -103,9 +117,11 @@ def euler_method(
     return times,solution_prey,solution_pred
 
 
-dT = 0.001
-t_stop = 60
-time, N1, N2 = euler_method(100,5,1.1,0.4,0.1,0.4,dT,t_stop)
+dT = 1
+t_stop = 100
+
+#Running Euler Method pred-prey
+time, N1, N2 = euler_method(N2initial, N1initial, 1, 2, 1, 3,0.05,t_stop)
 f, (ax1, ax2) = plt.subplots(2)
 
 ax1.plot(time, N1, color="b")
@@ -115,21 +131,79 @@ ax2.plot(time, N2, color="r")
 ax1.set_ylabel("Fish")
 ax2.set_ylabel("Bears")
 ax2.set_xlabel("Time")
-# Solve ODE
-y = solve_ivp(simulation, [0, 100], [10, 1], args=params, method="DOP853", max_step=dT)
 
 
-f, (ax1, ax2) = plt.subplots(2)
 
-time, N1, N2 = y.t, y.y[0, :], y.y[1, :]
 
-(line1,) = ax1.plot(time, N1, color="b")
 
-(line2,) = ax2.plot(time, N2, color="r")
 
-ax1.set_ylabel("Fish")
-ax2.set_ylabel("Bears")
-ax2.set_xlabel("Time")
+
+'''
+Solve the Lotka-Volterra competition and predator/prey equations using
+Scipy's ODE class and the adaptive step 8th order solver.
+Parameters
+----------
+simulation : function
+A python function that takes `time`, [`N1`, `N2`] as inputs and
+returns the time derivative of N1 and N2.
+
+N1_init, N2_init : float
+Initial conditions for `N1` and `N2`, ranging from (0,1]
+                                                    
+dT : float, default=10
+Largest timestep allowed in years.
+
+t_final : float, default=100
+Integrate until this value is reached, in years.
+
+args=params : float, alpha,beta,gamma,delta
+Lotka-Volterra coefficient values
+
+Returns
+-------
+time : Numpy array
+Time elapsed in years.
+
+N1, N2 : Numpy arrays
+Normalized population density solutions.
+'''
+
+# Solve Competition ODE with ivp
+zeta = solve_ivp(dNdt_comp, [0, t_stop], [N1initial,N2initial], args=params, 
+              method="DOP853", max_step=dT)
+
+# Solve Predator-Prey ODE with ivp
+y = solve_ivp(simulation, [0, t_stop], [N1initial,N2initial], args=params, 
+              method="DOP853", max_step=dT)
+
+
+
+f, (ax1) = plt.subplots(1,1, figsize = (10,6))
+
+timepp, N1pp, N2pp = y.t, y.y[0, :], y.y[1, :]
+
+(line1,) = ax1.plot(timepp, N1pp, color="b")
+
+(line2,) = ax1.plot(timepp, N2pp, color="r")
+
+ax1.set_title('Predator-Prey RK8')
+ax1.set_ylabel("Moose")
+
+
+
+plt.show()
+
+v, (ax3) = plt.subplots(1, 1, figsize = (10,6))
+
+timecomp, N1comp, N2comp = zeta.t, zeta.y[0, :], zeta.y[1, :]
+
+(line1,) = ax3.plot(timecomp, N1comp, color="b")
+
+(line2,) = ax3.plot(timecomp, N2comp, color="r")
+
+ax3.set_title('Competition RK8')
+ax3.set_ylabel("hey")
+
 
 
 plt.show()
